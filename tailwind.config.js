da4b51a11a7010110235de916b767b5f404f19f6
hp <86504012+hp252@users.noreply.github.com>
2022-10-15 02:44:07 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/dashboard.html", "./src/student-signin.html","./src/teacher-signin.html","./src/index.html","./src/chat.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
